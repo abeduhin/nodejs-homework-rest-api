3185bbb5e78a7be13a0ead6cca0453dab8ca3735
Oleksii Biedukhin <abeduhin17@gmail.com>
2023-03-07 10:25:59 +0200

    hw04-auth

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -1,20 +1,11 @@
 
-const { User } = require("../../models/users")
-const {RequestError} = require("../../helpers");
-
 const getCurrent = async (req, res) => {
-    const {email, subscription } = req.body;    
-    const user = await User.findOne({email});
-    if(!user) {
-        throw RequestError(401, "Not authorized")
-    }    
-    
-    res.status(200).json({
-        subscription,
-        email,
-    })
-}
+  const { email, subscription } = req.user;
+
+  res.json({
+    email,
+    subscription,
+  });
+};
 
 module.exports = getCurrent;
-
-
