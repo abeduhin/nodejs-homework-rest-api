9e4a4914a53d75e20b706389222f35308d3837de
Oleksii Biedukhin <abeduhin17@gmail.com>
2023-03-10 22:34:39 +0200

    HW05-avatars

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,5 @@
 const bcrypts = require("bcryptjs")
+const gravatar = require("gravatar")
 
 const { User } = require("../../models/users")
 
@@ -10,8 +11,9 @@ const register = async(req, res)=> {
     if(user){
         throw RequestError(409, "Email in use")
     }
-    const hashPassword = await bcrypts.hash(password, 10)
-    const result = await User.create({ email, subscription, password: hashPassword });
+    const hashPassword = await bcrypts.hash(password, 10);
+    const avatarURL = gravatar.url(email);
+    const result = await User.create({ email, subscription, password: hashPassword, avatarURL});
     res.status(201).json({        
     email: result.email,
     subscription: result.subscription,  
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,8 +1,10 @@
 const validateBody = require("./validateBody");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
 module.exports = {
     validateBody,
     authenticate,
+    upload,
 }
 // Елспортуємо
